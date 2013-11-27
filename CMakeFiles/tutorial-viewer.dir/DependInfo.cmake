4e08d3e2e1e41f917fc1b704fef28d62dd864785
pgivens4915 <pgivens4915@gmail.com>
2013-11-27 14:00:04 -0700

    Uploading all

---
+++ CMakeFiles/tutorial-viewer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paul/projects/visp/tutorial-viewer.cpp" "/home/paul/projects/visp/CMakeFiles/tutorial-viewer.dir/tutorial-viewer.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "UNIX"
+  "BOOST_ALL_NO_LIB"
+  "UNIX"
+  "BOOST_ALL_NO_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/OGRE"
+  "/usr/include/ois"
+  "/usr/include/libxml2"
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ tutorial-viewer.cpp
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <visp/vpMbEdgeTracker.h>
+#include <visp/vpMeEllipse.h>
+#include <visp/vpKltOpencv.h>
+#include <visp/vpDisplayOpenCV.h>
+#include <visp/vpOpenCVGrabber.h>
+#include <visp/vpDot2.h>
+
+int main(int argc, char** argv){
+    int device = 0;
+    cv::VideoCapture cap(device);
+
+    cv::Mat frame;
+    cap >> frame;
+    
+    //vpImage<vpRGBa> I;
+    vpImage<unsigned char> I;
+    vpImageConvert::convert(frame, I);
+    vpDisplayOpenCV d(I);
+    vpHomogeneousMatrix cMo;
+    vpCameraParameters cam;
+
+    vpDisplay::display(I);
+    vpDisplay::flush(I);
+
+    vpMbEdgeTracker tracker;
+
+    vpMe me;
+    me.setMaskSize(5);
+    me.setMaskNumber(180);
+    me.setRange(8);
+    me.setThreshold(10000);
+    me.setMu1(0.5);
+    me.setMu2(0.5);
+    me.setSampleStep(4);
+    me.setNbTotalSample(250);
+    tracker.setMovingEdge(me);
+    cam.initPersProjWithoutDistortion(839, 839, 325, 243);
+    tracker.setCameraParameters(cam);
+    tracker.setNearClippingDistance(0.1);
+    tracker.setFarClippingDistance(100.0);
+    tracker.setClipping(tracker.getClipping() | vpMbtPolygon::FOV_CLIPPING);
+    tracker.setDisplayFeatures(true);
+    tracker.setOgreVisibilityTest(true);
+    tracker.setAngleAppear(70);
+    tracker.setAngleDisappear(80);
+    tracker.loadModel("teabox.cao");
+    //tracker.loadModel("teabox.wrl");
+    tracker.initClick(I, "teabox.init");
+
+
+    for(;;){
+        try {
+        cap >> frame;
+        vpImageConvert::convert(frame,I);
+        vpDisplay::display(I);
+
+        tracker.track(I);
+        tracker.getPose(cMo);
+        tracker.getCameraParameters(cam);
+        tracker.display(I, cMo, cam, vpColor::red, 2);
+        vpDisplay::displayFrame(I, cMo, cam, 0.025, vpColor::none, 3);
+        vpDisplay::flush(I);
+        if (vpDisplay::getClick(I, false))
+            break;
+        }
+        catch(vpTrackingException){
+            printf("BLEHHHH");
+            return -1;
+        }
+    }
+}
